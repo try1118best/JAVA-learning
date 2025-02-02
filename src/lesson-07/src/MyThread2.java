59531997cd04cb424c4a8884fb4379492d709c29
try1118best <2187718660@qq.com>
2025-02-02 21:35:06 +0800

    第7天

---
+++ src/lesson-07/src/MyThread2.java
@@ -0,0 +1,8 @@
+public class MyThread2 extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("张三打取快递" + i);
+        }
+    }
+}
