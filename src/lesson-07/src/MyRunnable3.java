5938ef68b60f021b6c75bd9e96057e873cb0ccab
try1118best <86912419+try1118best@users.noreply.github.com>
2025-03-03 21:27:45 +0800

    Delete src directory

--- src/lesson-07/src/MyRunnable3.java
+++
@@ -1,8 +0,0 @@
-public class MyRunnable3 implements Runnable{
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(".....去买饭.....");
-        }
-    }
-}
