bd96dae6cd80d14d6ea637c687163e726c266c24
try1118best <86912419+try1118best@users.noreply.github.com>
2025-03-03 21:27:45 +0800

    Delete src directory

--- src/lesson-07/src/MyRunnable1.java
+++
@@ -1,8 +0,0 @@
-public class MyRunnable1 implements Runnable{//实现类
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("........打水");
-        }
-    }
-}
--- src/lesson-07/src/MyThread2.java
+++
@@ -1,8 +0,0 @@
-public class MyThread2 extends Thread{
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("张三打取快递" + i);
-        }
-    }
-}
