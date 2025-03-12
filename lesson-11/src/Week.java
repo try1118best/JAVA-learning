99bb7a8a7c7c7686bfb9ee6190b001a061818775
try1118best <2187718660@qq.com>
2025-03-12 14:42:14 +0800

    3.11:  lesson-11:枚举类 + Math类

---
+++ lesson-11/src/Week.java
@@ -0,0 +1,12 @@
+public class Week {
+    public static final int MONDAY = 0;
+    public static final int TUESDAY = 1;
+    public static final int WEDNESDAY = 2;
+    public static final int THURSDAY = 3;
+    public static final int FRIDAY = 4;
+    public static final int SATURDAY = 5;
+    public static final int SUNDAY = 6;
+
+    private Week() {
+    }
+}
